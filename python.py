321bca916054d76ccafcaa22a513da3283395dc5
BOB <jacohen@siue.edu>
2015-10-22 20:12:04 -0500

    TEST

---
+++ python.py
@@ -0,0 +1,25 @@
+print( " STARTED!" )
+
+word = "BOOKS"
+
+print( word[0:5] )
+
+i = 0
+
+while( i < 3 ):
+
+    print( i )
+
+    i += 1;
+
+for i in word:
+
+    print( i )	
+
+def PrintStuff( setWord ):
+
+    print( "IN FUNCTION!" )
+
+    print( setWord )
+
+PrintStuff( word )
